9a79b150c62fa222b3eb227b41eb4f14a96dd2c4
ZhangYanzhe <2712885306@qq.com>
2019-01-18 16:12:07 +0800

    query中企业模块的思考

---
+++ src/components/query/EnterpriseDemonstrate.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="enterprise">
+    <div>
+      {{enterpriseName}}
+    </div>
+    <div>
+      {{enterpriseDescribe}}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.enterprise{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
+
+<script>
+  export default {
+    name: 'EnterpriseDemonstrate',  
+    props:{
+      enterpriseName: String,
+      enterpriseDescribe: String
+    },
+    data(){
+      return{
+
+      }
+    },
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
--- src/views/Query.vue
+++ src/views/Query.vue
@@ -6,6 +6,20 @@
     <div class="query-background">
       <img src="../assets/QueryImgs/background.jpg" alt="">
     </div>
+    <div class="enterprise-row">
+      <EnterpriseDemonstrate
+        enterpriseName="dasdad"
+        enterpriseDescribe="aldhasd"
+      ></EnterpriseDemonstrate>
+      <EnterpriseDemonstrate
+        enterpriseName="dasdad"
+        enterpriseDescribe="aldhasd"
+      ></EnterpriseDemonstrate>
+      <EnterpriseDemonstrate
+        enterpriseName="dasdad"
+        enterpriseDescribe="aldhasd"
+      ></EnterpriseDemonstrate>
+    </div>
   </div>
 </template>
 
@@ -17,14 +31,24 @@
 }
 .query-background{
   position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.enterprise-row{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 </style>
 
 <script> 
   import QueryBanner from '@/components/query/banner.vue'
+  import EnterpriseDemonstrate from '@/components/query/EnterpriseDemonstrate.vue'
   export default {
     components:{
-      QueryBanner
+      QueryBanner,
+      EnterpriseDemonstrate
     },
     data() {
       return {

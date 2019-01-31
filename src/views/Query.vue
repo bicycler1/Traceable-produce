67c1736dca214390ebc48a19398882d93e200daa
ZhangYanzhe <2712885306@qq.com>
2019-01-31 11:15:51 +0800

    query样式

--- src/views/Query.vue
+++ src/views/Query.vue
@@ -9,6 +9,22 @@
     <div class="query-input">
       <div>
         <div class="query-input-banner">
+          <i class="fa fa-search"></i>
+          溯源查询
+        </div>
+        <div class="query-qr-input">
+          <div class="query-input-left">
+            <div class="qr-code">
+              <img src="../assets/QueryImgs/qrCode.png"/>
+              <div>
+                &nbsp;&nbsp;立刻扫码下载<br>
+                &nbsp;&nbsp;溯源移动APP
+              </div>
+            </div>
+            <div class="query-input-right">
+              adshada
+            </div>
+          </div>
         </div>
       </div>
       <div>
@@ -17,24 +33,24 @@
     </div>
     <div class="enterprise-row">
       <EnterpriseDemonstrate
-        enterpriseName="智慧农业软件平台"
-        enterpriseDescribe="智诚乐创"
-        enterpriseImg="1"
+      enterpriseName="智慧农业软件平台"
+      enterpriseDescribe="智诚乐创"
+      enterpriseImg="1"
       ></EnterpriseDemonstrate>
       <EnterpriseDemonstrate
-        enterpriseName="智慧农业软件平台"
-        enterpriseDescribe="智诚乐创"
-        enterpriseImg="2"
+      enterpriseName="智慧农业软件平台"
+      enterpriseDescribe="智诚乐创"
+      enterpriseImg="2"
       ></EnterpriseDemonstrate>
       <EnterpriseDemonstrate
-        enterpriseName="智慧农业软件平台"
-        enterpriseDescribe="智诚乐创"
-        enterpriseImg="1"
+      enterpriseName="智慧农业软件平台"
+      enterpriseDescribe="智诚乐创"
+      enterpriseImg="1"
       ></EnterpriseDemonstrate>
       <EnterpriseDemonstrate
-        enterpriseName="智慧农业软件平台"
-        enterpriseDescribe="智诚乐创"
-        enterpriseImg="2"
+      enterpriseName="智慧农业软件平台"
+      enterpriseDescribe="智诚乐创"
+      enterpriseImg="2"
       ></EnterpriseDemonstrate>
     </div>
   </div>
@@ -72,9 +88,38 @@
   width: 20%;
 }
 .query-input-banner{
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
   background-color: #f3f3f3;
   height: 50px;
   border-top: 1px solid #62d800; 
+  font-size: 1.2rem;
+  color: #666;
+  margin-bottom: 16px;
+}
+.query-input-banner>i{
+  margin: 0 6px;
+  color: #62d800;
+}
+.query-qr-input{
+  position: relative;
+}
+.query-input-left{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 30%;
+  padding: 6px;
+}
+.qr-code{
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.qr-code>img{
+  width: 100px;
+  height: 100px;
 }
 .enterprise-row{
   display: flex;

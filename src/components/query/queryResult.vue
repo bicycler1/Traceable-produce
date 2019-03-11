201ac04b8a90a530bc9e87ea9bf8a31a5421fba6
ZhangYanzhe <2712885306@qq.com>
2019-03-11 22:13:38 +0800

    加入queryResult页面

---
+++ src/components/query/queryResult.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+ export default {
+   name:
+ }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
